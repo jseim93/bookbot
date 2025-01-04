37e49b9c16025270ed91801af7173f7bd2c14b80
jseim93 <jseim93@gmail.com>
2025-01-03 22:43:39 -0600

    word count

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello world")
+def main():
+    book_path = "github.com/jseim93/bookbot/books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document.")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+    
+main()
